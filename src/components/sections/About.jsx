d4179f87feda5c34458bc77c6c0e46a13271d86b
ousafa <ouaamou.safa@gmail.com>
2025-03-18 18:33:00 +0000

    Reveal on scroll

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,5 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
 export const About = () => {
   const frontendSkills = ["React", "JavaScript", "TailwindCSS", "Git"];
   const backendSkills = ["PHP", "Laravel", "Python", "MySQL"];
@@ -6,96 +8,98 @@ export const About = () => {
       id="about"
       className="min-h-screen flex items-center justify-center py-20"
     >
-      <div className="max-w-3xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-orange-600 to-amber-700 bg-clip-text text-transparent text-center">
-          {" "}
-          About Me
-        </h2>
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-orange-600 to-amber-700 bg-clip-text text-transparent text-center">
+            {" "}
+            About Me
+          </h2>
 
-        <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-          <p className="text-gray-300 mb-6">
-            A motivated fresh graduate, passionate about leveraging skills and
-            knowledge to build a successful and impactful career in the tech
-            industry.
-          </p>
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              A motivated fresh graduate, passionate about leveraging skills and
+              knowledge to build a successful and impactful career in the tech
+              industry.
+            </p>
 
-          {/* Skills */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {/* Frontend */}
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> Frontend</h3>
-              <div className="flex flex-wrap gap-2">
-                {frontendSkills.map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-orange-500/10 text-orange-500 py-1 px-3 rounded-full text-sm hover:bg-orange-500/20 
+            {/* Skills */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {/* Frontend */}
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-orange-500/10 text-orange-500 py-1 px-3 rounded-full text-sm hover:bg-orange-500/20 
                                     hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
                     "
-                  >
-                    {tech}
-                  </span>
-                ))}
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              {/* Backend */}
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Backend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {backendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-orange-500/10 text-orange-500 py-1 px-3 rounded-full text-sm hover:bg-orange-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
-            {/* Backend */}
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> Backend</h3>
-              <div className="flex flex-wrap gap-2">
-                {backendSkills.map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-orange-500/10 text-orange-500 py-1 px-3 rounded-full text-sm hover:bg-orange-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
-                    "
-                  >
-                    {tech}
-                  </span>
-                ))}
+          </div>
+
+          {/* Education  & Experience*/}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              {/* Education */}
+              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong>
+                    {" "}
+                    Specialized Technician Diploma in Computer Engineering{" "}
+                  </strong>{" "}
+                  - Higher School of Marrakech (2024-2025)
+                </li>
+                <li>
+                  <strong>
+                    {" "}
+                    Bachelor's Degree in Fundamental Studies in Life Sciences{" "}
+                  </strong>{" "}
+                  -Semlalia Faculty of Sciences (2023-2024)
+                </li>
+              </ul>
+            </div>
+
+            {/* Experience */}
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 💼 Activities </h3>
+              <div className="space-y-4 text-gray-300">
+                <div>
+                  <h4 className="font-semibold"> LeetCode </h4>
+                  <p>Solving 10 problems.</p>
+                </div>
+
+                <div>
+                  <h4 className="font-semibold"> SalamHack </h4>
+                  <p>Participation in the SalamHack 2025 hackathon.</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-        {/* Education  & Experience*/}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-            {/* Education */}
-            <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
-            <ul className="list-disc list-inside text-gray-300 space-y-2">
-              <li>
-                <strong>
-                  {" "}
-                  Specialized Technician Diploma in Computer Engineering{" "}
-                </strong>{" "}
-                - Higher School of Marrakech (2024-2025)
-              </li>
-              <li>
-                <strong>
-                  {" "}
-                  Bachelor's Degree in Fundamental Studies in Life Sciences{" "}
-                </strong>{" "}
-                -Semlalia Faculty of Sciences (2023-2024)
-              </li>
-            </ul>
-          </div>
-
-          {/* Experience */}
-          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4"> 💼 Activities </h3>
-            <div className="space-y-4 text-gray-300">
-              <div>
-                <h4 className="font-semibold"> LeetCode </h4>
-                <p>Solving 10 problems.</p>
-              </div>
-
-              <div>
-                <h4 className="font-semibold"> SalamHack </h4>
-                <p>Participation in the SalamHack 2025 hackathon.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      </RevealOnScroll>
     </section>
   );
 };
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,21 +1,25 @@
+import { RevealOnScroll } from "../RevealOnScroll";
 export const Home = () => {
   return (
     <section
       id="home"
       className="min-h-screen flex items-center justify-center relative"
     >
-      <div className="text-center z-10 px-4">
-        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-orange-600 to-amber-700 bg-clip-text text-transparent leading-right">
-          Hi, I'm Safa Ouammou
-        </h1>
+      <RevealOnScroll>
+        <div className="text-center z-10 px-4">
+          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-orange-600 to-amber-700 bg-clip-text text-transparent leading-right">
+            Hi, I'm Safa Ouammou
+          </h1>
 
-        <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
-        I am a recent graduate full-stack developer, eager to achieve my goals through gaining extensive experience.
+          <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
+            I am a recent graduate full-stack developer, eager to achieve my
+            goals through gaining extensive experience.
           </p>
 
           <div className="flex justify-center space-x-4">
             <a
-              href="https://drive.google.com/file/d/1HvUNV8CsSC7VwH17zzUEcNDouH6slXdm/view?usp=sharing" target="_blank"
+              href="https://drive.google.com/file/d/1HvUNV8CsSC7VwH17zzUEcNDouH6slXdm/view?usp=sharing"
+              target="_blank"
               className="bg-orange-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(255, 20, 147, 0.4)]"
             >
               View Resume
@@ -28,7 +32,8 @@ export const Home = () => {
               Contact Me
             </a>
           </div>
-      </div>
+        </div>
+      </RevealOnScroll>
     </section>
   );
 };
